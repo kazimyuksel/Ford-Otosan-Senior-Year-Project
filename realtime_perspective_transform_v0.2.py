eca4e8475ad8b6d5fbae71a672edc4715fa1f42c
kazimyuksel <39468105+kazimyuksel@users.noreply.github.com>
2018-11-18 13:11:38 +0300

    Update realtime_perspective_transform_v0.2.py

--- realtime_perspective_transform_v0.2.py
+++ realtime_perspective_transform_v0.2.py
@@ -6,45 +6,12 @@ assert cv2.__version__[0] == '3', 'The program requires OpenCV version greater t
 import numpy as np
 import os
 from threading import Thread
-
 from picamera.array import PiRGBArray
 from picamera import PiCamera
-
-class PiVideoStream:
-	def __init__(self, resolution=(640, 480), framerate=32):
-		# initialize the camera and stream
-		self.camera = PiCamera()
-		self.camera.resolution = resolution
-		self.camera.framerate = framerate
-		self.rawCapture = PiRGBArray(self.camera, size=resolution)
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
- 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
-		self.frame = None
-		self.stopped = False
-    
-	def start(self):
-		# start the thread to read frames from the video stream
-		Thread(target=self.update, args=()).start()
-		return self
- 
-	def update(self):
-		# keep looping infinitely until the thread is stopped
-		for f in self.stream:
-			# grab the frame from the stream and clear the stream in
-			# preparation for the next frame
-			self.frame = f.array
-			self.rawCapture.truncate(0)
- 
-			# if the thread indicator variable is set, stop the thread
-			# and resource camera resources
-			if self.stopped:
-				self.stream.close()
-				self.rawCapture.close()
-				self.camera.close()
-				return
+import imutils
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
+import argparse
     
 ###########################################
 # MPU6050 RELATED REGISTERS AND FUCNTIONS
@@ -86,6 +53,16 @@ def get_x_rotation(x,y,z):
     return math.degrees(radians)
 
 ###########################################
+#CAMERA PARAMETERS
+
+# Camera image dimension
+DIM=(640, 480)
+# Camera intrinsic matrix
+K_1=np.array([[335.5619374393186, 0.0, 320.0], [0.0, 333.5403907261929, 240.0], [0.0, 0.0, 1.0]])
+#Camera distorsion matrix
+D_1=np.array([[-0.01877824020864759], [-0.036627875068484854], [0.03601968351612183], [-0.012209616189402786]])
+
+###########################################
 #CAMERA FUNCTIONS
 
 # Undistort Image
@@ -105,24 +82,13 @@ def undistort(img_array, balance=1.0, dim2=None, dim3=None):
     undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
     return undistorted_img
 
-###########################################
-#CAMERA PARAMETERS
-
-# Camera image dimension
-DIM=(640, 480)
-# Camera intrinsic matrix
-K_1=np.array([[335.5619374393186, 0.0, 320.0], [0.0, 333.5403907261929, 240.0], [0.0, 0.0, 1.0]])
-#Camera distorsion matrix
-D_1=np.array([[-0.01877824020864759], [-0.036627875068484854], [0.03601968351612183], [-0.012209616189402786]])
-
-
 
 ###########################################
 ###########################################
 ###########################################
 # MAIN PROGRAM STARTS HERE
 
-CAMERA_FRAMERATE = 32
+CAMERA_FRAMERATE = 90
 CAMERA_HEIGHT_PIXEL = 480
 CAMERA_WIDTH_PIXEL = 640
 camera_H_angle = 82 #degree
@@ -141,3 +107,118 @@ address = 0x68 #MPU6050 i2c address
 
 # MPU6050 wakeup command
 bus.write_byte_data(address, power_mgmt_1, 0)
+
+
+stream = PiVideoStream(resolution=(CAMERA_WIDTH_PIXEL, CAMERA_HEIGHT_PIXEL), CAMERA_FRAMERATE=32).start()
+# Allow the camera to warmup
+time.sleep(2.0)
+
+start_time = 0
+avg_hz = 0
+
+H = CAMERA_HEIGHT_PIXEL - top_crop_pixel - bottom_crop_pixel
+W = CAMERA_WIDTH_PIXEL - left_crop_pixel - right_crop_pixel
+
+# Get raw accelaration data from MPU6050
+accel_xout = read_word_2c(0x3b)
+accel_yout = read_word_2c(0x3d)
+accel_zout = read_word_2c(0x3f)
+
+# Convert accelaration data to m/s^2
+accel_xout_scaled = accel_xout / 16384.0
+accel_yout_scaled = accel_yout / 16384.0
+accel_zout_scaled = accel_zout / 16384.0
+
+# Get initial camera angle
+camera_theta = 90 + get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
+
+while True:
+    accel_xout = read_word_2c(0x3b)
+    accel_yout = read_word_2c(0x3d)
+    accel_zout = read_word_2c(0x3f)
+    
+    accel_xout_scaled = accel_xout / 16384.0
+    accel_yout_scaled = accel_yout / 16384.0
+    accel_zout_scaled = accel_zout / 16384.0
+    
+    # Low Pass Filter
+    lpf_camera = 0.6
+    camera_theta = camera_theta*lpf_camera + (90 + get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled))*(1-lpf_camera)
+    
+    # Calculate side length of square to be transformed
+    L_ref = 2*camera_height*np.tan(np.radians(camera_H_angle)/5)
+    
+    # Calculate pixel per radians
+    h = H/np.radians(camera_H_angle)
+    w = W/np.radians(camera_W_angle)
+    
+    # Distance to center of camera
+    x_mid = camera_height/np.tan(np.radians(camera_theta))
+    
+    # Lateral Line's angle to midpoint
+    phi_1 = np.arccos((camera_height*camera_height + x_mid*(x_mid-L_ref/2))/((np.sqrt(np.square(camera_height)+np.square(x_mid)))*(np.sqrt(np.square(camera_height)+np.square(x_mid-L_ref/2)))))
+    phi_2 = np.arccos((camera_height*camera_height + x_mid*(x_mid+L_ref/2))/((np.sqrt(np.square(camera_height)+np.square(x_mid)))*(np.sqrt(np.square(camera_height)+np.square(x_mid+L_ref/2)))))
+    
+    # Longitudinal Line's angle to midpoint
+    beta_1 = np.arctan((L_ref/2)/(np.sqrt(np.square(x_mid-L_ref/2)+np.square(camera_height))))
+    beta_2 = np.arctan((L_ref/2)/(np.sqrt(np.square(x_mid+L_ref/2)+np.square(camera_height))))
+    
+    # Corresponing corner's pixel values
+    I_t = H/2 - compression_factor_H*phi_1*h
+    I_b = H/2 + compression_factor_H*phi_2*h
+    J_tr = W/2 + compression_factor_W*beta_2*w
+    J_tl = W/2 - compression_factor_W*beta_2*w
+    J_br = W/2 + compression_factor_W*beta_1*w
+    J_bl = W/2 - compression_factor_W*beta_1*w
+    
+    # Array of corner coordinates for line drawing
+    ### This part will not be useful in future
+    p_1_prime = (np.round(J_tr).astype(int),np.round(I_t).astype(int))
+    p_2_prime = (np.round(J_br).astype(int),np.round(I_b).astype(int))
+    p_3_prime = (np.round(J_bl).astype(int),np.round(I_b).astype(int))
+    p_4_prime = (np.round(J_tl).astype(int),np.round(I_t).astype(int))
+    
+    # Array of corner coordinates and transformed square corner coordinates
+    pts1 = np.float32([[J_tr,I_t],[J_br,I_b],[J_bl,I_b],[J_tl,I_t]])
+    pts2 = np.float32([[540,470],[540,530],[460,530],[460,470]])
+    
+    # Homology Matrix for transformation
+    matrix = cv2.getPerspectiveTransform(pts1,pts2)
+    image = vs.read()
+
+    image = undistort(image)
+    
+    # Crop image to given spesifications
+    image_2 = image[top_crop_pixel:(CAMERA_HEIGHT_PIXEL-bottom_crop_pixel),left_crop_pixel:(CAMERA_WIDTH_PIXEL-right_crop_pixel)]
+    
+    # Draw perspective square on image
+    ### Will be removed in future
+    cv2.line(image, p_1_prime, p_2_prime, (0,255,0), thickness=2, lineType=8, shift=0)
+    cv2.line(image, p_2_prime, p_3_prime, (0,255,0), thickness=2, lineType=8, shift=0)
+    cv2.line(image, p_3_prime, p_4_prime, (0,255,0), thickness=2, lineType=8, shift=0)
+    cv2.line(image, p_4_prime, p_1_prime, (0,255,0), thickness=2, lineType=8, shift=0)
+
+    # Show undistorted image with lines
+    cv2.imshow("Undistort", image)
+    
+    # Apply Inverse Perspective Transform
+    image_3 = cv2.warpPerspective(image_2, matrix, (1000,1000))
+    
+    #Show Inverse Perspective Transformed image
+    cv2.imshow("Advanced Undistort", image_3)
+	
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+    
+    # Calculate Looptime and Frequency
+    elapsed_time = time.time() - start_time
+    start_time = time.time()  
+    avg_hz = avg_hz*0.8 + 0.2/elapsed_time
+    print(avg_hz)
+	
+	
+cv2.destroyAllWindows()
+vs.stop()
+	
+
